6a86a1036199d708bb8a42cdf86386709acac0e5
Gina Napier <gina.napier@miraclesystems.net>
2022-03-18 01:15:42 -0400

    refactored JS code to use constructors to definestore values

--- js/app.js
+++ js/app.js
@@ -8,19 +8,14 @@ let storeResults=[];
 let storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
 
-//Define a shop using an book object constructor
-function shopCreator(storeName,minCustomers,maxCustomers,avgCookiePerSale,CxPerHour,results){
-this.storeName='';
-this.minCustomers = 0;
-this.maxCustomers = 0;
-this.avgCookiePerSale = 0;
-this.CxPerHour = function calculate(a,b){
-    //Create random # of customers
-    let ans= Math.floor(Math.random() * 12);
-    min = Math.ceil(a);
-    max = Math.floor(b);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-    };
+//Define a shop object constructor function
+function shop(storeName,minCustomers,maxCustomers,avgCookiePerSale,CxPerHour){
+this.storeName=storeName;
+this.minCustomers = minCustomers;
+this.maxCustomers = maxCustomers;
+this.avgCookiePerSale = avgCookiePerSale;
+this.cxPerHour = CxPerHour;
+this.results = [];
 };
 
 //Hold the shops in an array
@@ -31,95 +26,64 @@ let stores =[
 ['Paris',	20,	38,	2.3],
 ['Lima',	2,	16,	4.6] ];
 
-
+//For loop to iterate through the stores array
 for (let i = 0; i < stores.length; i++) {
   let storeName = stores[i][0];
   let min = stores[i][1];
   let max = stores[i][2];
   let avgCookie = Math.round(stores[i][3]);
-  let store = new shopCreator(storeName,min,max,avgCookie);
-  let CPH = store.CxPerHour(min,max);
-
- // Call the addElement function to add the store name
-  addElement(storeName);
+  let cxFunction = function calculate(a,b){
+    //Create random # of customers
+    min = Math.floor(a);
+    max = Math.ceil(b);
+    return Math.ceil(Math.random() * (max - min) + min);
+    };
+  let cxPerHour =cxFunction(min,max);
+  
+  
+  let newStore = new shop(storeName,min,max,avgCookie,cxPerHour);
  
-  //Push the temporary array into the storeData array
-  storeData.push(tempArray);
+console.log(newStore.storeName);
 
-  //console.log("Store Name:" + storeName);
-
-
-for (let index = 1; index < storeHours.length; index++) {
+//storeResults.push([newStore.storeName,newStore.minCustomers,newStore.maxCustomers,newStore.avgCookiePerSale,newStore.cxPerHour]);
   
-  //Get the right hour from the storeHours array
-  let hour = storeHours[index];
-  
-  //Calculate average cookies
-  let customers= store.CxPerHour(min,max);
-  
-  // Calculate the cookies for the hour 
-  let cookiesThisHour = Math.round(customers * avgCookie);
-
-  let text = hour + ': ' + cookiesThisHour + ' cookies.';
-
- // Push these calculated results to an array
- storeResults.push(text);
-
-
- addToList(text, storeName) 
-  
-  };  
- 
- //Clear the temporary array for use in the next iteration
-  console.table(storeResults);
-  
-
-  tempArray.push(storeResults);
-storeResults =[]; 
-};
-
-function addElement(storeName) {
-
-  let myStore = storeName;
-  //Set the value for the first window to add data too
-  let storeSection= document.getElementById('store-profiles');
-
-  let listSection= document.getElementsByTagName('ul');
- 
-  // Define new elements
-  const articleElem = document.createElement('article');
-  //articleElem.id = storeName;
-  const h2El = document.createElement('h2');
-  h2El.innerText=storeName;
-  const pEl = document.createElement('p');
-  const ulEl = document.createElement('ul');
-  ulEl.className=myStore;
-
-  const imgEl = document.createElement('img');
+  //Loop through the store hours and calculate cookies sold per hour
+  for (let index = 0; index < storeHours.length; index++) {
+    let time = storeHours[index];
+    //generate a random # of customers
+    let cxNum = cxFunction(newStore.minCustomers,newStore.maxCustomers);
+    //Define average cookie sales for this hour
+    let cookies = (newStore.avgCookiePerSale);
     
-  storeSection.appendChild(articleElem);
- 
- //Create new elements under the article
-  articleElem.appendChild(h2El);
-  articleElem.appendChild(pEl);
-  articleElem.appendChild(ulEl);
-  articleElem.appendChild(imgEl);
-};
+    //Display hour
+    console.log("Time: " + time);
+    //Display number of cookies
+    console.log("cookies: " + cookies);
+    //Display number of customers
+    console.log("customers: " + cxNum);
+    
+    //Define cookie results for this hour
+    cookies = cxNum * cookies;
+    
+    //Define  text resukts for this hour
+    let text = `${time}: ${cookies} cookies`;
+    //Display number of cookies for this hour
+    console.log("number of cookies: " + cookies);
+    //Determine num of cookies by average customer this hour
+    //console.log(cookies);
+    
+    //Store results in an array
+    storeResults.push(cookies);
 
-function addToList(item, storeName) {
-
-  let newItem = item;
-  let targetStore = storeName;
-  //Set the value for the first window to add data too
-  let listSection= document.querySelector('.' + targetStore)
-  console.log(listSection);
-  //let listSection= document.getElementsByTagName('ul');
- 
-  // Define new elements
-  const newEl = document.createElement('li');
-  newEl.innerText=item;
-
-  //Create new elements under the article  
-  listSection.appendChild(newEl);
+    //Call the list function to add this text to a list
+  };
   
+  //Push store text array to the 'results' variable in the store object
+  newStore.results=storeResults;
+  
+  //Empty array for next iteration
+  storeResults=[];
+//console.log(storeResults.storeName);
+ //get last value or array
+  console.table(newStore.results[14]); 
 };
